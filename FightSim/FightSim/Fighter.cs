d113cb515ec43b5e618d59ce15c31294a10d0e17
BeepBoopError <emil0131@gmail.com>
2019-10-09 17:55:33 +0200

    övning klar, inget i det frivilliga

---
+++ FightSim/FightSim/Fighter.cs
@@ -0,0 +1,47 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Fighter
+    {
+        private static Random rangen = new Random();
+
+        private int hp = 10;
+
+        public string name = "";
+
+        //Attack-metoden anropas när slagskämpen ska attackera någon, och returnerar den mängd skada hjälten utdelar.
+        public int Attack()
+        {
+            int damage = rangen.Next(1,11);
+            return damage;
+        }
+
+        //Hurt-metoden anropas när slagskämpen ska bli skadad. Den tar emot en mängd skada som parameter.
+        public void Hurt(int amount)
+        {
+            amount = Math.Abs(amount);
+            hp -= amount;
+            if (hp < 0) { hp = 0; }
+        }
+
+        //IsAlive anropas för att undersöka ifall slagskämpen har några hp kvar. Den returnerar true om så är fallet. Om det inte finns några hp kvar returneras false.
+        public bool IsAlive()
+        {
+            bool alive = true;
+            if (hp <= 0) { alive = false; }
+            return alive;
+        }
+
+        //GetHp-metoden anropas för att undersöka hur många hp slagskämpen har kvar. Den returnerar slagskämpens nuvarande hp-värde.
+        public int GetHp()
+        {
+            return hp;
+        }
+
+    }
+}
---
+++ FightSim/FightSim/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buckle up buckaroo, it's time to fight, please enter the name of the first fighter:");
+
+            Fighter A = new Fighter() { name = Console.ReadLine() };
+
+            Console.WriteLine("Now tippity type in the name for fighter two!");
+
+            Fighter B = new Fighter() { name = Console.ReadLine() };
+
+            bool fighting = true;
+
+            Console.WriteLine("It's fighting time!");
+
+            Console.ReadLine();
+
+            while (fighting)
+            {
+                //they hurt eachother for the others damage
+                B.Hurt(A.Attack());
+                A.Hurt(B.Attack());
+
+                //if they're both alive
+                if (A.IsAlive() && B.IsAlive())
+                {
+                    Console.WriteLine("Both are hurt! Owie");
+                    Console.WriteLine(A.name + " Has " + A.GetHp() + " Hitpoints left!");
+                    Console.WriteLine(B.name + " Has " + B.GetHp() + " Hitpoints left!");
+                    Console.WriteLine("They prepare for another round");
+                }
+                //if both die
+                else if (A.IsAlive() == false && B.IsAlive() == false)
+                {
+                    Console.WriteLine("Both combatants fall, too bad!");
+                    fighting = false;
+                }
+                //if B dies
+                else if (A.IsAlive() && B.IsAlive() == false)
+                {
+                    Console.WriteLine(A.name + " stands victorious, " + B.name + " falls!");
+                    fighting = false;
+                }
+                //if a dies
+                else
+                {
+                    Console.WriteLine(B.name + " stands victorious, " + A.name + " falls!");
+                    fighting = false;
+                }
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("end");
+
+            Console.ReadLine();
+        }
+    }
+}
